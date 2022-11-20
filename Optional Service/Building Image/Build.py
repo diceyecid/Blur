4a450c0d8e5345bac858150ab8001ac8133842f9
Oscar Chu <oscarchu19940113@gmail.com>
2022-11-20 13:43:24 -0800

    Building Service

--- Optional Service/Building Image/Build.py
+++ Optional Service/Building Image/Build.py
@@ -1,27 +1,28 @@
 # Building Image service
 
-import blur.web
-import blur.ioloop
+import tornado.web
+import tornado.ioloop
 
-class uploadHandler(blur.web.RequestHandler):
+class uploadImgHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
-    def post(self)
-        files = self.request.files["BlurFiles"]:
-        for f in files
-            fh = open(f"img/{f.filename}", "wb")
+    def post(self):
+        files = self.request.files["origin"]
+        for f in files:
+            fh = open(f"Img/{f.filename}", "wb")
             fh.write(f.body)
             fh.close()
-        self.write(f"http://localhost:4040/img/{f.filename}")
+        self.write(f"http://localhost:4040/Img/{f.filename}")
 
-if (__name__ == "__main__")"
-    app = blur.web.Application([
-        ("/", uploadHandler),
-        ("/img/(.*)", blur.web.StaticFileHandler, {"path" :"img"})
+
+if (__name__ == "__main__"):
+    app = tornado.web.Application([
+        ("/", uploadImgHandler),
+        ("/Img/(.*)", tornado.web.StaticFileHandler, {"path" :"Img"})
     ])
 
     app.listen(4040)
     print("Listening on port 4040")
 
-    blur.ioloop.IOloop.Instance().start()
+    tornado.ioloop.IOLoop.instance().start()
