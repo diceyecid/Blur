f91fd6d3402cfd7cbff15e1255515ceab7270755
Ross <49296963+diceyecid@users.noreply.github.com>
2022-11-19 18:57:19 -0800

    added gitignore for swift

---
+++ Blur Application/Blur Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blur Application
+//
+//  Created by Ross Shen on 2022-11-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
