34d43e367ea8c4c38b77e8a6cf56acd02e086f62
Ross <49296963+diceyecid@users.noreply.github.com>
2022-11-21 15:10:14 -0800

    added the redirection to image

--- Image Processing Service/server.py
+++ Image Processing Service/server.py
@@ -3,17 +3,27 @@
 import tornado.web
 import tornado.ioloop
 
+# server url
+URL = 'http://localhost:4040/'
+
+# upload image to the server
 class uploadImgHandler(tornado.web.RequestHandler):
+    # send upload portal to client
     def get(self):
         self.render("index.html")
 
+    # uplaod image to Img folder
     def post(self):
-        files = self.request.files["origin"]
-        for f in files:
+        if( 'image' in self.request.files ):
+            files = self.request.files["image"]
+            f = files[0]
             fh = open(f"Img/{f.filename}", "wb")
             fh.write(f.body)
             fh.close()
-        self.write(f"http://localhost:4040/Img/{f.filename}")
+            # self.write( URL + f"Img/{f.filename}")
+            self.redirect( URL + f'Img/{ f.filename }' )
+        else:
+            self.write( 'No image uploaded' )
 
 
 if (__name__ == "__main__"):
